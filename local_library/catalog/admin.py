f248081490bb7ab00d47fed953bbee74eb6b7619
Laksh <laksh8396@gmail.com>
2025-03-04 19:05:20 +0530

    Create and register models for the catalog

--- local_library/catalog/admin.py
+++ local_library/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from catalog.models import Author, Book, BookInstance, Genre, Language
+
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Language)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+
